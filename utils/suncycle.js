a2e4b9497e0c75a0b70343a3b598ee8e5ee91e3a
lucasrossell <lrossell517@gmail.com>
2023-04-25 17:03:25 -0700

    added adjustable sun speed and position

--- utils/suncycle.js
+++ utils/suncycle.js
@@ -1,8 +1,10 @@
 export function suncycle() {
   const amplitude = 5; // adjust the amplitude of the sine wave
   const frequency = 0.05; // adjust the frequency of the sine wave
-  const yOffset = window.gridSize.y / 2 - 1; // adjust the vertical offset of the sine wave
-  window.sunlight.x += 1;
+  const speed = parseFloat(document.getElementById("sunlight-speed").value);
+  let yOffset = parseInt(document.getElementById("sunlight-position").value);
+  yOffset = window.gridSize.y - yOffset;
+  window.sunlight.x += speed;
   window.sunlight.y =
     amplitude * Math.sin(frequency * window.sunlight.x) + yOffset;
   if (window.sunlight.x > window.gridSize.x) {
